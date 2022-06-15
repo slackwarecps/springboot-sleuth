ebfee4d454778b0f2f83bd15056df97d80f963c2
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-06-15 19:16:40 -0300

    Arquivo de configuracao adicionado

--- src/main/java/br/com/fabioalvaro/springbootsleuth/SleuthController.java
+++ src/main/java/br/com/fabioalvaro/springbootsleuth/SleuthController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Executor;
+
 @RestController
 public class SleuthController {
 
 
     @Autowired
     private SleuthService sleuthService;
+
+    @Autowired
+    private Executor executor;
 
 
     Logger logger = LoggerFactory.getLogger(SleuthController.class);
@@ -37,6 +42,26 @@ public class SleuthController {
         logger.info("A5 :: New Span :: success!!");
         return "success";
     }
+
+    @GetMapping("/new-thread")
+    public String helloSleuthNewThread() {
+        logger.info("New Thread");
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("I'm inside the new thread - with a new span");
+        };
+        executor.execute(runnable);
+
+        logger.info("I'm done - with the original span");
+        return "success";
+    }
+
+
+
 
 
 
