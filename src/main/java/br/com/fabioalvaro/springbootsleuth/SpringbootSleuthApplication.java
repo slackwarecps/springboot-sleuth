062535f9b3d0c155b363c6c506b3dd5fcb3feb5d
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-06-15 18:45:18 -0300

    Initial Commit :)

---
+++ src/main/java/br/com/fabioalvaro/springbootsleuth/SpringbootSleuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabioalvaro.springbootsleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSleuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSleuthApplication.class, args);
+    }
+
+}
